71c918bb1203a2f53d60e3e5e5d30ee1593d5f0c
akashin <kashin.andrej@gmail.com>
2021-04-24 13:56:21 +0100

    Remove logo

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -7,23 +7,11 @@ export default class Demo extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
     this.load.image('hammer', 'assets/hammer.png');
     this.load.image('submarine', 'assets/submarine.png');
   }
 
   create() {
-    const logo = this.add.image(400, 70, 'logo');
-
-    this.tweens.add({
-      targets: logo,
-      y: 350,
-      duration: 1500,
-      ease: 'Sine.inOut',
-      yoyo: true,
-      repeat: -1
-    });
-
     let submarine = new Submarine(this, {});
     this.add.existing(submarine);
   }

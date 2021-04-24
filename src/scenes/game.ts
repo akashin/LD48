6264144525e05390edbd87650798c863dcce348b
akashin <kashin.andrej@gmail.com>
2021-04-24 14:28:31 +0100

    Add basic actions

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -2,8 +2,11 @@ import Phaser from 'phaser';
 import { Submarine } from '../objects/submarine';
 
 export default class GameScene extends Phaser.Scene {
+  private actions: Array<Phaser.GameObjects.Text>;
+
   constructor() {
     super('GameScene');
+    this.actions = new Array<Phaser.GameObjects.Text>();
   }
 
   preload() {
@@ -12,7 +15,35 @@ export default class GameScene extends Phaser.Scene {
   }
 
   create() {
+    var gameWidth = this.game.config.width as number;
+    var gameHeight = this.game.config.height as number;
+
     let submarine = new Submarine(this, {});
     this.add.existing(submarine);
+
+    this.addAction('Explore', this.exploreAction);
+    this.addAction('Dive', this.diveAction);
+    this.addAction('Repair', this.repairAction);
+  }
+
+  addAction(text: string, onClick) {
+    var actionText = this.add.text(
+      10, 10 + (24 + 10) * this.actions.length, text, { color: 'white', fontSize: '24pt' }
+    )
+    actionText.setInteractive();
+    actionText.on('pointerup', onClick);
+    this.actions.push(actionText);
+  }
+
+  exploreAction(pointer: any) {
+    console.log("Explore!")
+  }
+
+  diveAction(pointer: any) {
+    console.log("Dive!")
+  }
+
+  repairAction(pointer: any) {
+    console.log("Repair!")
   }
 }
